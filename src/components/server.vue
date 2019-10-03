0bc916d1e8b1ff1f721def169e1023c4ff0b5b5a
Maurina_Davis <maurina.davis@gmail.com>
2019-10-03 08:15:39 -0600

    styling of page

--- src/components/server.vue
+++ src/components/server.vue
@@ -3,14 +3,14 @@
             <v-card class="mx-auto list-group-item cards" :key ="index" v-for="index in 5" max-width="344">
               <v-card-text>
                 <div>
-                  <ul class="list-group">
-                    server #{{ index }}
-                  </ul>
+               
+                  <h2>  Server #{{ index }} </h2>
+                
                 </div>
               </v-card-text>
               <v-card-actions>
                 <v-btn text color="teal accent-4"> 
-                  Learn More
+                  Test
                 </v-btn>
               </v-card-actions>
           
